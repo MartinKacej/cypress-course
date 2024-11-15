81679be48dad210f020b6e12276eb03ba5326263
Martin <m.kacej@atlas.sk>
2024-11-15 16:57:36 +0100

    added cucumber support

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,15 @@
 const { defineConfig } = require("cypress");
+const cucumber = require("cypress-cucumber-preprocessor").default;
 
 module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
+      on('file:preprocessor', cucumber())
     },
+      specPattern: "cypress/e2e/cucumber-tests/*.feature",
+      stepDefinitions: "cypress/integration",
       testIsolation: true,
       video:false,
       defaultCommandTimeout: 1000,
