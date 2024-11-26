6d55d1d329366eca8f5d534e8703c46e84167ec6
Martin <m.kacej@atlas.sk>
2024-11-26 21:12:02 +0100

    added API check to login test

---
+++ cypress/e2e/real_app/login.cy.js
@@ -0,0 +1,61 @@
+/// <reference types="Cypress"/>
+import LoginPage from "./pages/login.cy"
+
+describe('Check that RealWorld app is running', () => {
+
+    it('Assert URL', () => {
+        cy.exec('pgrep -f node.*cypress-realworld-app').its('stdout').should('not.equal', '')
+        cy.visit('localhost:3000')
+        cy.title().should('contain','Cypress Real World App')
+    })
+})
+
+describe('Try to sign with invalid credentials', () => {
+
+    Cypress.config('baseUrl','http://localhost:3000/signin')
+    
+    const loginPage = new LoginPage()
+
+    beforeEach("Visit logon page", () => {
+        loginPage.visit()
+    })
+ 
+    it('Empty credentials', () => {
+        cy.wait(500)
+        loginPage.userField().clear()
+        loginPage.passField().clear()
+        loginPage.submit()
+        loginPage.userWarning().should('be.visible')
+        
+    })
+    
+    it('Empty password', () => {
+        loginPage.userField().type('test')
+        loginPage.userWarning().should('not.exist')
+        loginPage.submitButton().should('be.disabled')
+    })
+    
+    it('Short password', () => {
+        loginPage.signIn("test","t")
+        loginPage.submitButton().should('be.disabled')
+        loginPage.passWarning().should('exist') 
+    })
+    
+    it('Non existent user', () => {
+        cy.intercept('POST','/login').as('req')
+        loginPage.signIn("test","test")
+        loginPage.passWarning().should('not.exist') 
+        cy.url().should('equal','http://localhost:3000/signin')
+        cy.get('.MuiAlert-message').contains('Username or password is invalid')
+        cy.wait('@req').then((request) => {
+            cy.log(request)
+            expect(request.response.statusCode).eq(401)
+            }
+        )
+    })
+
+    it('Check registration link', ()=> {
+        loginPage.regLink().click()
+        cy.url().should('equal','http://localhost:3000/signup')
+    })
+})
