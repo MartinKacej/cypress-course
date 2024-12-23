fa14ae7969a9623389866e75168f58487424a2b4
Martin <m.kacej@atlas.sk>
2024-12-23 23:43:05 +0100

    POM - user settings

---
+++ cypress/e2e/real_app/pages/user_settings.js
@@ -0,0 +1,33 @@
+/// <reference types="Cypress"/>
+export default class UserSettings {
+	visit() {
+		cy.visit('http://localhost:3000/user/settings')
+	}
+	name() {
+		return cy.get('[data-test="user-settings-firstName-input"]')
+	}
+	nameWarning() {
+		return cy.get('#user-settings-firstName-input-helper-text')
+	}
+	surname() {
+		return cy.get('[data-test="user-settings-lastName-input"]')
+	}
+	surnameWarning() {
+		return cy.get('#user-settings-lastName-input-helper-text')
+	}
+	getEmail() {
+		return cy.get('[data-test="user-settings-email-input"]')
+	}
+	emailWarning() {
+		return cy.get('#user-settings-email-input-helper-text')
+	}
+	phoneNumber() {
+		return cy.get('[data-test="user-settings-phoneNumber-input"]')
+	}
+	phoneWarning() {
+		return cy.get('#user-settings-phoneNumber-input-helper-text')
+	}
+	saveButton() {
+		return cy.get('[data-test="user-settings-submit"]')
+	}
+}
